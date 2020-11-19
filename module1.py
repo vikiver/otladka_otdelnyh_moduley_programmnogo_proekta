3b089c86d19c5d6a2e074ddf3b543a11f5fb1be6
testtk21 <73307045+testtk21@users.noreply.github.com>
2020-11-20 00:43:35 +0300

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,2 +1,4 @@
+#Вывод первого и последнего элементов списка
+
 def first_and_last_item(inputList):
-    print('First list item: ', inputList[0], '\nLast list item ', inputList[-1])
+    print('First list item: ', inputLis[0], '\nLast list item ', inputLis[1])
