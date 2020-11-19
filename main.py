4c54d3339940d0e60cac7b2bcb1b8b9311d91874
testtk21 <73307045+testtk21@users.noreply.github.com>
2020-11-20 00:42:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import module1
-import module2
+import module2.py
 import module3
 
 print('List created...\n')
@@ -16,11 +16,11 @@ print('\n1 - Print the first and last list item')
 print('2 - Print the sum of all items')
 print('3 - Print amount of all list items')
 print('\nSelect an option by typing the character: ')
-option = int(input())
+option = int(input()
 
 if option == 1:
-     module1.first_and_last_item(userList)
+     module1.first_and_last_ite(userList)
 elif option == 2:
-     module2.list_sum(userList)
-elif option == 3:
+     module2.lis_sum(userList)
+elif option == 4:
      module3.amount_of_all_items(userList)
