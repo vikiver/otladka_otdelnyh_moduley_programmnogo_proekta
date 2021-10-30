c420bf79de912c1ee8c9c8a9264507abce52ef7a
testtk21 <73307045+testtk21@users.noreply.github.com>
2021-10-30 18:08:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import module1.py
 import module2.py
 import module3.py
+import module4.py
+import module5.py
 
 print('Список создан...\n')
 
 userList = [] 
 while True:
-     listElem = input('Введите элемент списка. Введите пробел, если хотите закончить создание списка ')
+     listElem = int(input('Введите элемент списка. Введите пробел, если хотите закончить создание списка '))
      if listElem == '' or listElem == ' ':
           break
      else:
-          userList.append(int(listElem))
+          userList.append(listElem)
 
 print('\n1 - Вывести первый и последний элемент списка')
 print('2 - Вывести сумму всех элементов')
 print('3 - вывести количество элементов')
+print('4 - вывести все чётные элементы')
+print('5 - вывести все нечётные элементы')
 print('\nВыберите операцию, которую хотите выполнить. Для этого введите номер операции: ')
 option = int(input()
 
 if option == 1:
-     module1.first_and_last_ite(userList)
+     module1.first_and_last_items(userList)
 elif option == 2:
      module2.lis_sum(userList)
-elif option == 4:
+elif option == 3:
      module3.amount_of_all_items(userList)
+elif option == 4:
+     module3.even_items(userList)
+elif option == 5:
+     module3.odd_items(userList)
