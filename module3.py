e176c157a276eb4a6846bd85026e117fafab5f40
testtk21 <73307045+testtk21@users.noreply.github.com>
2021-11-03 11:50:44 +0300

    Update module3.py

--- module3.py
+++ module3.py
@@ -1,5 +1,5 @@
 #Вывод количества элементов в списке
 
-def amount_of_all_items(inputList)
+def amount_of_all_items(inputList):
     #Вставьте свой код здесь
     print('Количество всех элементов списка: ')
