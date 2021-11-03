88b94c0528c14b26d8c2efab7d1ff5b90147b6a3
vikiver <91251250+vikiver@users.noreply.github.com>
2021-11-03 17:47:53 +0300

    Update module5.py

--- module5.py
+++ module5.py
@@ -3,3 +3,7 @@
 def odd_items(inputList):
     #Вставьте свой код здесь
     print('Нечётные элементы списка: ')
+    return print('Нечётные элементы списка: ', [x for x in inputList if x % 2 != 0])
+ 
+     
+     
