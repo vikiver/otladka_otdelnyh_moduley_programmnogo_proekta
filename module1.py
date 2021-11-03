c2138ac8da083d8f4537030480068c1d19323653
vikiver <91251250+vikiver@users.noreply.github.com>
2021-11-03 17:28:36 +0300

    Update module1.py

--- module1.py
+++ module1.py
@@ -2,5 +2,6 @@
 
 def first_and_last_items(inputList):
     #Вставьте свой код здесь
-    print('Первый элемент списка: ')
-    print('Последний элемент списка: ')
+    print('Первый элемент списка: ', inputList[0])
+    print('Последний элемент списка: ', inputList[-1])
+
